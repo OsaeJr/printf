17ebf50a282bd528ef8476c6f09b34a11b7dab88
SamuelAsiedu <mybusinessconcepts@gmail.com>
2023-07-25 18:00:05 -0700

    rewrite printf

--- printf1.c
+++ printf1.c
@@ -7,49 +7,68 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int(*function)(va_list, char *, unsigned int);
-	char *buffer;
+	int count = 0;
+	char buffer[20];
+	int num,i;
+	va_list args;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) *1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
-		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
+	va_start(args, format);
+	while (*format)
 	{
-		if (format[i] == '%')
+		if (*format == '%')
 		{
-			if (format[i + 1] == '\0')
-			{
-				print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
-			}
-			else
-			{
-				function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			}
+			format++;
 
-			i++;
+			switch (*format)
+			{
+				case 'c':
+					putchar(va_arg(args, int));
+					count++;
+					break;
+
+				case 's':
+				{
+					const char *str = va_arg(args, const char *);
+					while (*str)
+					{
+						putchar(*str);
+						str++;
+						count++;
+					}
+					break;
+				}
+				case '%':
+					putchar('%');
+					count++;
+					break;
+					case 'd': case 'i': {
+                    
+                    num = va_arg(args, int);
+                    sprintf(buffer, "%d", num);
+                    for (i = 0; buffer[i] != '\0'; i++) {
+                        putchar(buffer[i]);
+                        count++;
+                    }
+                    break;
+                }
+                		default:
+					putchar('%');
+					putchar(*format);
+					count += 2;
+					break;
+			}
+	
 		}
 		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-		;
-	}
+		{
+			putchar(*format);
+			count++;
+		}
 
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+		format++;
+    }
+
+	va_end(args);
+	return count;
 }
+
